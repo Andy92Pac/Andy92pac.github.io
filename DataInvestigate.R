0ee80a96e56d627d4a1dfd55f171957def0d4944
Bylal <farid.ahamada25@gmail.com>
2018-01-12 16:59:33 +0100

    script R de test et visualisation de nos données

---
+++ DataInvestigate.R
@@ -0,0 +1,5 @@
+# Database connection
+conCapteurs = mongo(collection = "capteurs", db="trafic", url = "mongodb://193.51.82.104:2343")
+conCapteurs_geo = mongo(collection = "capteurs_geo", db="trafic", url = "mongodb://193.51.82.104:2343")
+conTrafic = mongo(collection = "trafic", db="trafic", url = "mongodb://193.51.82.104:2343")
+
