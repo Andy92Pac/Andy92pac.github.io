36bc77885cfce43e5c5a811052b67ff97e36cc32
Bylal <farid.ahamada25@gmail.com>
2018-01-12 15:14:22 +0100

    Shiny dashboard structure

---
+++ ui.R
@@ -0,0 +1,43 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+
+# Define UI for application that draws a histogram
+shinyUI(
+  ui <- dashboardPage(
+    dashboardHeader(title = "Basic dashboard"),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+        menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+      )
+    ),
+    dashboardBody(
+      tabItems(
+        # First tab content
+        tabItem(tabName = "dashboard",
+                fluidRow(
+                  box(plotOutput("plot1", height = 250)),
+                  
+                  box(
+                    title = "Controls",
+                    sliderInput("slider", "Number of observations:", 1, 100, 50)
+                    )
+                  )
+                ),
+        # Second tab content
+        tabItem(tabName = "widgets",
+                h2("Widgets tab content")
+        )
+        )
+      )
+    )
+  )
