eca898184517099666095100b42c2a1b6fbcb44a
andy92pac <andy.mpondo@gmail.com>
2018-05-01 14:04:59 +0200

    update

--- global.R
+++ global.R
@@ -16,23 +16,4 @@ cap = mongo(collection = "capteurs", db = "trafic",
             url = "mongodb://193.51.82.104:2343")
 tra = mongo(collection = "trafic", db = "trafic", 
             url = "mongodb://193.51.82.104:2343")
-# adresse = read.csv("adresse_paris.csv", row.names = NULL, header = T, sep = ";")
 
-
-plotByDay = renderPlot(
-  selectedCap %>% 
-    mutate(heure = format(date, "%H"),
-           jour = format(date, "%A")) %>%
-    group_by(heure, jour) %>%
-    summarise(moy = mean(tauxNum, na.rm = TRUE)) %>%
-    ggplot(aes(heure, moy, col = jour, group = jour)) +
-    geom_line() +
-    theme_classic()
-)
-k = tra$find(limit = 100)
-deb <- ts(k$debit)
-plot(deb)
-tmpserie.sanstend <- diff(deb)
-d = diff(tmpserie.sanstend) 
-
-plot(tmpserie.sanstend)
